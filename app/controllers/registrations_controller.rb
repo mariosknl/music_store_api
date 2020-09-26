89adeb9c06332807d3263bcfd373bdc3b067323c
mariosknl <mariosknl@outlook.com>
2020-09-26 17:06:47 +0300

    cookies created

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     profile = Admin.create(admin_params) if params[:admin]
-
+    profile = Guest.create(guest_params) if params[:guest]
     @user = User.new(sign_up_params)
     @user.profile = profile
 
     if @user.save
+      token = encode_token({ user_id: @user.id, token: 'token' })
+      cookies[:token] = { value: token, httponly: true, same_site: :none }
       render 'registrations/signup.json.jbuilder'
     else
       render json: { message: @user.errors.full_messages }
@@ -16,5 +18,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def admin_params
     params.require(:admin).permit(:username)
+  end
+
+  def guest_params
+    params.require(:guest).permit(:username)
   end
 end
--- app/views/registrations/signup.json.jbuilder
+++ app/views/registrations/signup.json.jbuilder
@@ -2,3 +2,8 @@ if @user.profile_type == 'Admin'
   json.email @user.email
   json.username @user.profile.username
 end
+
+if @user.profile_type == 'Guest'
+  json.email @user.email
+  json.username @user.profile.username
+end
