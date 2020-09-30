ff1496d6bd765d6fb92e3e2e85d2328f72039746
mariosknl <mariosknl@outlook.com>
2020-09-30 17:02:52 +0300

    updated cors file

--- app/views/sessions/logged?.json.jbuilder
+++ app/views/sessions/logged?.json.jbuilder
@@ -2,6 +2,7 @@ if @user.profile_type == 'Admin'
   json.email @user.email
   json.username @user.profile.username
   json.profile_type @user.profile_type
+  json.likes @user.likes
 end
 
 if @user.profile_type == 'Guest'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,4 +16,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       expose: ['Authorization'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins 'https://emusicstore.netlify.app'
+
+    resource '*',
+      headers: :any,
+      credentials: true,
+      expose: ['Authorization'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
