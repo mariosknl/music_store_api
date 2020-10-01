5998482a4a13c455a5e637367d150b7703dc028a
mariosknl <mariosknl@outlook.com>
2020-10-01 17:52:22 +0300

    updated cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://emusicstore.netlify.app/'
+    origins 'https://emusicstore.netlify.app'
 
     resource '*',
       headers: :any,
